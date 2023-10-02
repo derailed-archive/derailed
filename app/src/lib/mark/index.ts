a5a262ff6b522625a548b594d071f446db314f29
VincentRPS <vincentbusiness55@gmail.com>
2023-10-02 21:21:54 +0800

    fix: fixes to UI's API Client

---
+++ app/src/lib/mark/index.ts
@@ -0,0 +1,10 @@
+// this is used *before* markdown parsing to clean out
+// HTML without removing it.
+const escapeHtml = (unsafe: String) => {
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
--- app/src/pages/Register.tsx
+++ app/src/pages/Register.tsx
@@ -1,11 +1,11 @@
 import { useState, FormEvent } from "react"
 import { Navigate, useNavigate } from "react-router-dom"
-import superjson from "superjson"
+import JSON from "json-bigint"
 import { User } from "../types"
 
-interface LoginResponse {
-    token: string,
+interface RegisterResponse {
     user: User,
+    token: string
 }
 
 export default () => {
@@ -16,6 +16,8 @@ export default () => {
     const navigate = useNavigate()
     const [username, setUsername] = useState<string | null>(null)
     const [password, setPassword] = useState<string | null>(null)
+    const [displayAlert, setDisplay] = useState<boolean>(false)
+    const [alert, setAlert] = useState<string>("")
 
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
@@ -24,7 +26,7 @@ export default () => {
 
         try {
             let resp = await fetch(
-                API_URL.concat("/login"),
+                API_URL.concat("/register"),
                 {
                     method: "POST",
                     body: JSON.stringify({
@@ -38,37 +40,41 @@ export default () => {
                 },
             )
 
-            if (resp.status == 200) {
-                const text = await resp.text()
-                let json = superjson.parse<LoginResponse>(text)
-                console.log(text)
-                console.log(json)
-                console.log(json.token)
-                localStorage.setItem("token", json['token'])
+            if (resp.status == 201) {
+                let json = JSON.parse(await resp.text()) as RegisterResponse
+                localStorage.setItem("token", json.token)
                 navigate("/")
             } else {
                 console.error(await resp.text())
-                let err_json = superjson.parse<object>(await resp.text())
+                let err_json = JSON.parse(await resp.text()) as object
 
-                if ('code' in err_json && err_json.code === 2004) {
-                    // TODO!: show this to the user.
-                    console.error("Invalid password")
-                } else if ('code' in err_json && err_json.code === 2003) {
-                    // TODO!: show this to the user.
-                    console.error("Invalid username")
+                if ('code' in err_json && err_json.code === 2002) {
+                    setAlert("Username taken")
+                    setDisplay(true)
                 }
             }
-        } catch(err) {
-            console.log(err)
-            navigate("/error?code=unknown")
+        // @ts-ignore
+        } catch(err: Error) {
+            setAlert(err)
+            setDisplay(true)
         }
     }
 
     return (
         <main className="bg-no-repeat antialiased font-primary m-auto text-white bg-cover min-h-screen flex flex-col justify-center items-center" style={{backgroundImage: "url('/assets/trains-away.jpg')"}}>
+            {displayAlert && (
+                <div className="w-screen bg-quite-blue text-white">
+                    <h2>
+                        An error occurred!
+                    </h2>
+                    <h4>
+                        {alert}
+                    </h4>
+                </div>
+            )}
             <div className="bg-quite-blue rounded p-7 rounded-3">
                 <h1 className="font-semibold select-none text-2xl pb-2 max-w-sm text-center text-blackbird">
-                    Welcome back to <div className="text-unrailed">Derailed!</div>
+                    Welcome to <div className="text-unrailed">Derailed!</div>
                 </h1>
 
                 <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
@@ -82,11 +88,13 @@ export default () => {
                             <input className="rounded bg-quite-more-blue border-0 p-2 text-white" type="password" id="password" minLength={1} maxLength={70} onChange={(event) => {setPassword(event.target.value)}} required />
                         </div>
                     </section>
-                    <button type="submit" className="text-white outline-0 text-lg hover:bg-unrailed font-primary font-400 rounded-2 py-1 px-20 bg-quite-more-blue">
-                            Login
+                    <button type="submit" className="text-white text-lg hover:bg-unrailed font-primary font-400 rounded-2 py-1 px-20 bg-quite-more-blue">
+                            Register
                     </button>
                     <div className="text-blackbird font-semibold pt-2 text-sm">
-                        Don't have an account? <a href="/register" className="no-underline text-unrailed">Register</a> instead.
+                        Already have an account? <a href="/login" className="no-underline text-unrailed">
+                            Login
+                        </a> instead.
                     </div>
                 </form>
             </div>
